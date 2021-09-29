fad32916e7bb6de9567c528044323d8afd84330b
Gheorghe Morari <53918731+GheorgheMorari@users.noreply.github.com>
2021-09-29 10:08:20 +0300

    add usage to add_input_neuron()

--- main.cpp
+++ main.cpp
@@ -45,9 +45,10 @@ void manual_creation() {
 
 int main() {
 	//manual_creation();
+	srand(69);
 
-	Network n = Network({ 3,3,3,4 });
-	vector<float> input_vector = { 0.01,-0.1,-1.2 };
+	Network n = Network({ 3,3,2,2 });
+	vector<float> input_vector = { -0.01,0.1, 0.01 };
 	printf("Automatic network creation:\n");
 	printf("Input neurons:\n");
 	for (auto input : input_vector) {
@@ -64,6 +65,23 @@ int main() {
 		printf("%f\n", output);
 	}
 
+	n.add_input_neuron();
+	input_vector.push_back(-3);
+	printf("Expanded neural network:\n");
+	printf("Input neurons:\n");
+	for (auto input : input_vector) {
+		printf("%f\n", input);
+	}
+
+	n.propagate(input_vector);
+	printf("Output neurons:\n");
+	for (auto output : n.get_result()) {
+		printf("%f\n", output);
+	}
+	printf("Softmaxed output neurons:\n");
+	for (auto output : Softmax::GetInstance().get_result()) {
+		printf("%f\n", output);
+	}
 	
 
 
